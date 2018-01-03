5c18e1ca1fb3fe89fa7ca404ebcee6aa54b403f2
shujianhua <shujh@zyitong.net>
2018-01-03 17:47:32 +0800

    first init .

---
+++ src/org/jansu/now/object/StaticFactory.java
@@ -0,0 +1,15 @@
+package org.jansu.now.object;
+
+public class StaticFactory {
+
+    public static void main(String[] args){
+        System.out.println("hello");
+    }
+
+
+
+    public static StaticFactory getInstance(){
+        return
+    }
+
+}
